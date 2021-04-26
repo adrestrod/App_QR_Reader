e4dff1d00e0ce3ee98930fb5d6a5df92417f9aab
Adrian Estevez Rodriguez <adrestrod123@gmail.com>
2021-04-26 12:09:05 +0200

    modified icons

--- lib/widgets/scan_tiles.dart
+++ lib/widgets/scan_tiles.dart
@@ -27,8 +27,8 @@ class ScanTiles extends StatelessWidget {
               child: ListTile(
                 leading: Icon(
                     this.tipo == 'http'
-                        ? Icons.home_outlined
-                        : Icons.map_outlined,
+                        ? Icons.web_sharp
+                        : Icons.pin_drop_outlined,
                     color: Theme.of(context).primaryColor),
                 title: Text(scans[i].valor),
                 subtitle: Text(scans[i].id.toString()),
